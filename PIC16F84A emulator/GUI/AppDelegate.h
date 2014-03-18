7c2f56e3c4ae003aed81041bd56ea7191afd37ea
Asger Drewsen <asgerdrewsen@gmail.com>
2014-03-18 21:40:55 +0100

    Make GUI functional

--- PIC16F84A emulator/GUI/AppDelegate.h
+++ PIC16F84A emulator/GUI/AppDelegate.h
@@ -8,13 +8,24 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface AppDelegate : NSObject <NSApplicationDelegate, NSTableViewDataSource>
+@interface AppDelegate : NSObject <NSApplicationDelegate, NSTableViewDataSource, NSTextFieldDelegate>
 
 @property (assign) IBOutlet NSWindow *window;
-@property (weak) IBOutlet NSTableView *functionList;
+@property (weak) IBOutlet NSTableView *labelTable;
+@property (weak) IBOutlet NSTableView *variableInputTable;
+@property (weak) IBOutlet NSTableView *variableOutputTable;
+@property (weak) IBOutlet NSTextField *WInput;
+@property (weak) IBOutlet NSTextField *WOutput;
+@property (weak) IBOutlet NSButton *runButton;
 
 - (IBAction)open:(id)sender;
+- (IBAction)run:(id)sender;
 
-@property NSArray *functions;
+@property NSMutableArray *labels;
+@property NSMutableArray *labelAddresses;
+@property NSMutableArray *variables;
+@property NSMutableArray *variableAddresses;
+@property NSMutableArray *variableInputValues;
+@property NSMutableArray *variableOutputValues;
 
 @end
--- PIC16F84A emulator/GUI/AppDelegate.m
+++ PIC16F84A emulator/GUI/AppDelegate.m
@@ -9,14 +9,32 @@
 #import "AppDelegate.h"
 
 #include "load.h"
+#include "symbols.h"
+#include "state.h"
+#include "runtime.h"
+
+NSNumber *NSNumberFromString(NSString *str) {
+	return @(strtoll([str UTF8String], NULL, 0));
+}
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	self.labels = [[NSMutableArray alloc] init];
+	self.labelAddresses = [[NSMutableArray alloc] init];
+	self.variables = [[NSMutableArray alloc] init];
+	self.variableAddresses = [[NSMutableArray alloc] init];
+	self.variableInputValues = [[NSMutableArray alloc] init];
+	self.variableOutputValues = [[NSMutableArray alloc] init];
 	
-	self.functions = @[];
+	self.labelTable.dataSource = self;
+	self.variableInputTable.dataSource = self;
+	self.variableOutputTable.dataSource = self;
 	
-	self.functionList.dataSource = self;
+	self.WInput.delegate = self;
+	self.WOutput.delegate = self;
+	
+	[self.window orderOut:self];
 }
 
 - (IBAction)open:(id)sender {
@@ -34,15 +52,169 @@
 }
 
 - (void)loadFile:(NSURL *)url {
+	NSFileManager *fileManager = [NSFileManager defaultManager];
+	
+	NSString *projectName = [[url pathComponents] lastObject];
+	
+	NSString *distPrefix = [NSString stringWithFormat:@"%@/dist/default/production/%@.production", [url path], projectName];
+	
+	BOOL shouldLoad = YES;
+	
+	for(NSString *extension in @[@"map", @"hex"]) {
+		NSString *filePath = [NSString stringWithFormat:@"%@.%@", distPrefix, extension];
+		if(![fileManager fileExistsAtPath:filePath]) {
+			NSAlert *alert = [NSAlert alertWithMessageText:@"Error loading project" defaultButton:@"OK" alternateButton:nil otherButton:nil informativeTextWithFormat:@"The file '%@' could not be found. Please make sure you have built your project with MPLABX!", filePath];
+			[alert runModal];
+			
+			shouldLoad = NO;
+			break;
+		}
+	}
+	
+	if(!shouldLoad) {
+		return;
+	}
+	
 	load_files([[url path] UTF8String]);
+	
+	[self.labels removeAllObjects];
+	[self.labelAddresses removeAllObjects];
+	[self.variables removeAllObjects];
+	[self.variableAddresses removeAllObjects];
+	[self.variableInputValues removeAllObjects];
+	[self.variableOutputValues removeAllObjects];
+	
+	struct symbol *s, *tmp;
+	
+	HASH_ITER(hh, prog_symbols, s, tmp) {
+		NSString *name = [NSString stringWithUTF8String:s->name];
+		[self.labels addObject:name];
+		[self.labelAddresses addObject:@(s->address)];
+	}
+	
+	HASH_ITER(hh, ram_symbols, s, tmp) {
+		NSString *name = [NSString stringWithUTF8String:s->name];
+		[self.variables addObject:name];
+		[self.variableAddresses addObject:@(s->address)];
+		[self.variableInputValues addObject:[NSNull null]];
+		[self.variableOutputValues addObject:[NSNull null]];
+	}
+	
+	[self.labelTable reloadData];
+	[self.variableInputTable reloadData];
+	[self.variableOutputTable reloadData];
+	
+	[self.window makeKeyAndOrderFront:self];
+}
+
+- (IBAction)run:(id)sender {
+	[self.runButton setEnabled:NO];
+	
+	initialize();
+	reset();
+	
+	NSInteger selectedRow = self.labelTable.selectedRow;
+	if(selectedRow == -1) {
+		NSAlert *alert = [NSAlert alertWithMessageText:@"Error running" defaultButton:@"OK" alternateButton:nil otherButton:nil informativeTextWithFormat:@"Please select a label on the left before running!"];
+		[alert runModal];
+		return;
+	}
+	
+	PC = [self.labelAddresses[selectedRow] unsignedIntValue];
+	W = [NSNumberFromString(self.WInput.stringValue) unsignedIntValue];
+	for(int i = 0; i < [self.variables count]; i++) {
+		id value = self.variableInputValues[i];
+		if([value isKindOfClass:[NSNumber class]]) {
+			ram[[self.variableAddresses[i] unsignedIntValue]] = [value unsignedIntValue];
+		}
+	}
+	
+	run();
+	
+	self.WOutput.stringValue = [NSString stringWithFormat:@"0x%X", W];
+	for(int i = 0; i < [self.variables count]; i++) {
+		self.variableOutputValues[i] = @(ram[[self.variableAddresses[i] unsignedIntValue]]);
+	}
+	
+	[self.variableOutputTable reloadData];
+	
+	[self.runButton setEnabled:YES];
 }
 
 - (NSInteger)numberOfRowsInTableView:(NSTableView *)aTableView {
-	return [self.functions count];
+	if(aTableView == self.labelTable) {
+		return [self.labels count];
+	} else {
+		return [self.variables count];
+	}
 }
 
-- (id)tableView:(NSTableView *)tableView objectValueForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row {
-	return self.functions[row];
+- (id)tableView:(NSTableView *)aTableView objectValueForTableColumn:(NSTableColumn *)aTableColumn row:(NSInteger)rowIndex {
+	NSString *headerTitle =((NSTableHeaderCell *)aTableColumn.headerCell).title;
+	NSArray *array;
+	
+	if(aTableView == self.labelTable) {
+		if([headerTitle isEqualToString:@"Address"]) {
+			array = self.labelAddresses;
+		} else {
+			array = self.labels;
+		}
+	} else {
+		if([headerTitle isEqualToString:@"Address"]) {
+			array = self.variableAddresses;
+		} else if([headerTitle isEqualToString:@"Variable"]) {
+			array = self.variables;
+		} else if([headerTitle isEqualToString:@"Input Value"]) {
+			array = self.variableInputValues;
+		} else {
+			array = self.variableOutputValues;
+		}
+	}
+	
+	id element = array[rowIndex];
+	
+	if([element isKindOfClass:[NSString class]]) {
+		return element;
+	} else if([element isKindOfClass:[NSNull class]]) {
+		return @"?";
+	} else if([element isKindOfClass:[NSNumber class]]) {
+		return [NSString stringWithFormat:@"0x%X", [element unsignedIntValue]];
+	} else {
+		return nil;
+	}
+}
+
+- (void)tableView:(NSTableView *)aTableView setObjectValue:(id)anObject forTableColumn:(NSTableColumn *)aTableColumn row:(NSInteger)rowIndex {
+	id value;
+	
+	if([anObject isEqualToString:@""]) {
+		value = [NSNull null];
+	} else {
+		value = NSNumberFromString(anObject);
+		if([value unsignedIntValue] > 255) {
+			value = @(255);
+		}
+	}
+	
+	self.variableInputValues[rowIndex] = value;
+}
+
+- (BOOL)control:(NSControl *)control textShouldBeginEditing:(NSText *)fieldEditorn{
+	return control == self.WInput;
+}
+
+- (BOOL)control:(NSControl *)control textShouldEndEditing:(NSText *)fieldEditor {
+	if(control == self.WInput) {
+		NSNumber *number = NSNumberFromString(self.WInput.stringValue);
+		unsigned int value = [number unsignedIntValue];
+		if(value > 255) {
+			value = 255;
+		}
+		
+		self.WInput.stringValue = [NSString stringWithFormat:@"0x%X", value];
+	}
+	
+	return YES;
 }
 
 @end
